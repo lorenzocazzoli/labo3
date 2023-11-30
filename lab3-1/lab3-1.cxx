3cdef2edb97ae9d0c9e5f7aae2fa0d0a8cc0f137
mudslinger <lorenzo.cazzoli6@studio.unibo.it>
2023-11-30 17:47:20 +0100

    Inseriti valori del germanio, problema scala graph germanio

--- lab3-1/lab3-1.cxx
+++ lab3-1/lab3-1.cxx
@@ -40,20 +40,20 @@ void calibrazione() {
 // ! Vanno rinominati gli assi e cambiato il titolo del grafico
 void silicio() {
     // array di valori I
-    Double_t correnteMulti[17] {0.01, 0.02, 0.05, 0.07, 0.11, 0.14, 0.16, 0.25, 0.36, 0.53, 0.76, 0.89, 1.11, 1.65, 1.92, 3.97, 7.99};
+    Double_t correnteMultiS[17] {0.01, 0.02, 0.05, 0.07, 0.11, 0.14, 0.16, 0.25, 0.36, 0.53, 0.76, 0.89, 1.11, 1.65, 1.92, 3.97, 7.99};
     // array di logI          
-    Double_t correnteMultiLog[17];            
+    Double_t correnteMultiLogS[17];            
     // riempimento array di valori logI
     for(int i=0; i<17; i++) {
-        correnteMultiLog[i] = log(correnteMulti[i]);
+        correnteMultiLogS[i] = log(correnteMultiS[i]);
     }
-    Double_t erroreCorrenteMulti[17] {0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01};
-    Double_t voltaggioOscillo[17] {400.0, 450.0, 500.0, 520.0, 540.0, 550.0, 560.0, 580.0, 600.0, 620.0, 640.0, 650.0, 660.0, 680.0, 700.0, 750.0, 800.0};
-    Double_t erroreVoltaggioOscillo[17] {0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.2, 0.2, 0.2};
+    Double_t erroreCorrenteMultiS[17] {0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01};
+    Double_t voltaggioOscilloS[17] {400.0, 450.0, 500.0, 520.0, 540.0, 550.0, 560.0, 580.0, 600.0, 620.0, 640.0, 650.0, 660.0, 680.0, 700.0, 750.0, 800.0};
+    Double_t erroreVoltaggioOscilloS[17] {0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.2, 0.2, 0.2};
     TGraphErrors * silicioIV = new TGraphErrors(17);
     for(int i=0; i<17; i++) {
-        silicioIV->SetPoint(i, voltaggioOscillo[i], correnteMultiLog[i]);
-        silicioIV->SetPointError(i, erroreVoltaggioOscillo[i], erroreCorrenteMulti[i]);
+        silicioIV->SetPoint(i, voltaggioOscilloS[i], correnteMultiLogS[i]);
+        silicioIV->SetPointError(i, erroreVoltaggioOscilloS[i], erroreCorrenteMultiS[i]);
     }
     TF1 * silicioFunction = new TF1("silicioFunction", "[0]+[1]*x");
     silicioFunction->SetRange(540.0, -1.0, 650.0, -0.06);
@@ -65,28 +65,29 @@ void silicio() {
     silicioIV->Draw();
 }
 
-// ! I dati inseriti al momento sono quelli del silicio. Da cambiare al più presto
 // ! Gli errori sono sbagliati, vanno controllate le formule per ottenere gli errori
 // ! Vanno rinominati gli assi e cambiato il titolo del grafico
+// ! Per qualche motivo il range degli assi è completamente sballato e va a centinaia anche sulle y
 void germanio() {
     // array di valori I
-    Double_t correnteMulti[17] {0.01, 0.02, 0.05, 0.07, 0.11, 0.14, 0.16, 0.25, 0.36, 0.53, 0.76, 0.89, 1.11, 1.65, 1.92, 3.97, 7.99};
+    Double_t correnteMultiG[16] {0.01, 0.02, 0.03, 0.04, 0.07, 0.11, 0.18, 0.26, 0.40, 0.58, 0.84, 1.21, 1.70, 2.34, 3.08, 5.17};
     // array di logI          
-    Double_t correnteMultiLog[17];            
+    Double_t correnteMultiLogG[16];            
     // riempimento array di valori logI
-    for(int i=0; i<17; i++) {
-        correnteMultiLog[i] = log(correnteMulti[i]);
+    for(int i=0; i<16; i++) {
+        correnteMultiLogG[i] = log(correnteMultiG[i]);
     }
-    Double_t erroreCorrenteMulti[17] {0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01};
-    Double_t voltaggioOscillo[17] {400.0, 450.0, 500.0, 520.0, 540.0, 550.0, 560.0, 580.0, 600.0, 620.0, 640.0, 650.0, 660.0, 680.0, 700.0, 750.0, 800.0};
-    Double_t erroreVoltaggioOscillo[17] {0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.2, 0.2, 0.2};
+    // TODO controllare il range dell'asse Y!
+    Double_t erroreCorrenteMultiG[16] {0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01};
+    Double_t voltaggioOscilloG[16] {70, 90, 110, 130, 150, 170, 190, 210, 230, 250, 270, 290, 310, 330, 350, 400};
+    Double_t erroreVoltaggioOscilloG[16] {0.05, 0.05, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.2, 0.2, 0.5};
     TGraphErrors * germanioIV = new TGraphErrors(17);
     for(int i=0; i<17; i++) {
-        germanioIV->SetPoint(i, voltaggioOscillo[i], correnteMultiLog[i]);
-        germanioIV->SetPointError(i, erroreVoltaggioOscillo[i], erroreCorrenteMulti[i]);
+        germanioIV->SetPoint(i, voltaggioOscilloG[i], correnteMultiLogG[i]);
+        germanioIV->SetPointError(i, erroreVoltaggioOscilloG[i], erroreCorrenteMultiG[i]);
     }
     TF1 * germanioFunction = new TF1("germanioFunction", "[0]+[1]*x");
-    germanioFunction->SetRange(540.0, -1.0, 650.0, -0.06);
+    germanioFunction->SetRange(100.0, -1.0, 300.0, 0.0);
     germanioFunction->SetParameters(0, 0.5);
     germanioIV->Fit("germanioFunction", "R"); // R option limits the fit in the range of the function
     // ricordarsi di cambiare i nomi degli assi inserendo le appropriate unità di misura
