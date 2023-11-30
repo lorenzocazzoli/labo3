5281cbf88759424cf9f9aac070d8c33063e378f3
mudslinger <lorenzo.cazzoli6@studio.unibo.it>
2023-11-30 15:44:32 +0100

    Aggiunta funzione germanio, segnati TODO come commenti sopra le funzioini

--- lab3-1/lab3-1.cxx
+++ lab3-1/lab3-1.cxx
@@ -14,6 +14,7 @@ void setstyle() {
     gStyle->SetOptFit(1111);
 }
 
+// ! Vanno rinominati gli assi e cambiato il titolo del grafico
 void calibrazione() {
     Double_t calibroMultiV[8] {98.0 , 196.0 , 295.0 , 396.0 , 497.0 , 595.0 , 683.0, 784.0};
     // l'errore sul multimetro è trascurabile rispetto a quello sull'oscilloscopio
@@ -28,15 +29,16 @@ void calibrazione() {
     }
     TF1 * correlazione = new TF1("correlazione", "[0]+[1]*x", 98, 800);
     correlazione->SetParameters(0,1);
-    voltaggioVoltaggio->Fit("correlazione");
+    voltaggioVoltaggio->Fit("correlazione", "R"); // R option limits the fit in the range of the function
     // ricordarsi di cambiare i nomi degli assi inserendo le appropriate unità di misura
     // devono essere mV e mV, perché se no si hanno un po di problemini
     TCanvas * ccalibrazione = new TCanvas();
     voltaggioVoltaggio->Draw();
 }
 
+// ! Gli errori sono sbagliati, vanno controllate le formule per ottenere gli errori
+// ! Vanno rinominati gli assi e cambiato il titolo del grafico
 void silicio() {
-    gStyle->SetOptFit(0000);
     // array di valori I
     Double_t correnteMulti[17] {0.01, 0.02, 0.05, 0.07, 0.11, 0.14, 0.16, 0.25, 0.36, 0.53, 0.76, 0.89, 1.11, 1.65, 1.92, 3.97, 7.99};
     // array di logI          
@@ -56,21 +58,54 @@ void silicio() {
     TF1 * silicioFunction = new TF1("silicioFunction", "[0]+[1]*x");
     silicioFunction->SetRange(540.0, -1.0, 650.0, -0.06);
     silicioFunction->SetParameters(0, 0.5);
-    silicioIV->Fit("silicioFunction");
+    silicioIV->Fit("silicioFunction", "R"); // R option limits the fit in the range of the function
     // ricordarsi di cambiare i nomi degli assi inserendo le appropriate unità di misura
     // devono essere mV e mA
     TCanvas * cSilicio = new TCanvas(); 
     silicioIV->Draw();
 }
 
+// ! I dati inseriti al momento sono quelli del silicio. Da cambiare al più presto
+// ! Gli errori sono sbagliati, vanno controllate le formule per ottenere gli errori
+// ! Vanno rinominati gli assi e cambiato il titolo del grafico
 void germanio() {
-
+    // array di valori I
+    Double_t correnteMulti[17] {0.01, 0.02, 0.05, 0.07, 0.11, 0.14, 0.16, 0.25, 0.36, 0.53, 0.76, 0.89, 1.11, 1.65, 1.92, 3.97, 7.99};
+    // array di logI          
+    Double_t correnteMultiLog[17];            
+    // riempimento array di valori logI
+    for(int i=0; i<17; i++) {
+        correnteMultiLog[i] = log(correnteMulti[i]);
+    }
+    Double_t erroreCorrenteMulti[17] {0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01};
+    Double_t voltaggioOscillo[17] {400.0, 450.0, 500.0, 520.0, 540.0, 550.0, 560.0, 580.0, 600.0, 620.0, 640.0, 650.0, 660.0, 680.0, 700.0, 750.0, 800.0};
+    Double_t erroreVoltaggioOscillo[17] {0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.2, 0.2, 0.2};
+    TGraphErrors * germanioIV = new TGraphErrors(17);
+    for(int i=0; i<17; i++) {
+        germanioIV->SetPoint(i, voltaggioOscillo[i], correnteMultiLog[i]);
+        germanioIV->SetPointError(i, erroreVoltaggioOscillo[i], erroreCorrenteMulti[i]);
+    }
+    TF1 * germanioFunction = new TF1("germanioFunction", "[0]+[1]*x");
+    germanioFunction->SetRange(540.0, -1.0, 650.0, -0.06);
+    germanioFunction->SetParameters(0, 0.5);
+    germanioIV->Fit("germanioFunction", "R"); // R option limits the fit in the range of the function
+    // ricordarsi di cambiare i nomi degli assi inserendo le appropriate unità di misura
+    // devono essere mV e mA
+    TCanvas * cGermanio = new TCanvas(); 
+    germanioIV->Draw();
 }
 
 void analisi() {
+    /* impostazione grafica, con stile "Modern" e opzioni di visualizzazione del fit
+       1111 (non mi ricordo cosa voglia dire e non ho voglia di controllare)*/
     setstyle();
     /* fit V-V con sull'asse x il voltaggio ottenuto sul multimetro
        e sull'asse y il voltaggio ottunto sull'oscilloscopio     */
     calibrazione();
+    /* fit V-I con sull'asse x il voltaggio ottenuto sull'oscilloscopio
+       e sull'asse y la corrente ottunta sul multimetro          */
     silicio();
+    /* fit V-I con sull'asse x il voltaggio ottenuto sull'oscilloscopio
+       e sull'asse y la corrente ottunta sul multimetro          */
+    germanio();
 }
