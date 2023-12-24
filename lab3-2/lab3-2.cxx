02d456ecc3fc19a72e6a1b7dfd0715c24e6430ec
Lorenzo Cazzoli <lorenzo.cazzoli6@studio.unibo.it>
2023-12-24 01:47:30 +0100

    cambiato formato di stampa delle canvas in png

--- lab3-2/lab3-2.cxx
+++ lab3-2/lab3-2.cxx
@@ -93,7 +93,7 @@ void correnteTransistor() {
     TCanvas * canvas200 = new TCanvas(); // costruttore di base perché chi se ne frega
     studio200->SetTitle("Curve caratteristiche Vce-Ic; -Vce (Volt); -Ic (μA)");
     studio200->Draw("APCE");
-    canvas200->Print("bjt200.pdf");
+    canvas200->Print("bjt200.png");
     std::cout << "Ib = 200 μA - RISULTATI" << "\n";
     std::cout << "Valore Bf: " << funzione200->GetParameter("A")/200.0 << "\n";
     std::cout << "Valore Va: " << funzione200->GetParameter("A")/funzione200->GetParameter("B") << "\n";
@@ -105,7 +105,7 @@ void correnteTransistor() {
     TCanvas * canvas100 = new TCanvas(); // costruttore di base perché chi se ne frega
     studio100->SetTitle("Curve caratteristiche Vce-Ic; -Vce (Volt); -Ic (μA)");
     studio100->Draw("APCE");
-    canvas100->Print("bjt100.pdf");
+    canvas100->Print("bjt100.png");
     std::cout << "Ib = 100 μA - RISULTATI" << "\n";
     std::cout << "Valore Bf: " << funzione100->GetParameter("A")/200.0 << "\n";
     std::cout << "Valore Va: " << funzione100->GetParameter("A")/funzione100->GetParameter("B") << "\n";
